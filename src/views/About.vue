252dab337348a3190c853abf9505d250cde71201
375816003 <375816003@qq.com>
2019-07-24 10:01:02 +0800

    部分配置

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,5 +2,20 @@
   <div class="about">
     <img src="/static/logo.png" alt="" srcset="">
     <h1>This is an about page222</h1>
+    <HelloWorld></HelloWorld>
   </div>
 </template>
+<script>
+import HelloWorld from 'components/HelloWorld'
+export default {
+  data () {
+    return {
+      msg:'ss'
+    }
+  },
+  components:{
+    HelloWorld
+  }
+}
+</script>
+
